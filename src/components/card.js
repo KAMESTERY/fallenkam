3c4344f532c70e8aca4aae075a71b6e628d444bc
dtaylor-us <derektaylor.us@gmail.com>
2020-06-14 06:02:17 -0500

    add grayscale img class + toggle grayscale on hover

--- src/components/card.js
+++ src/components/card.js
@@ -7,15 +7,15 @@ const Card = ({data}) => {
   const toggleHover = () => setHovered(!hovered);
   let hoverClass = hovered ? '' : 'grayscale-img';
   return (
-      <div className="fl w-100 w-50-m w-25-ns pa2">
+      <div className="fl w-100 w-50-m w-25-ns pa2"
+           onMouseEnter={toggleHover}
+           onMouseLeave={toggleHover}>
         <article className="hide-child relative center">
           <img
               style={{cursor: 'pointer'}}
               src={mediaFtImage === undefined || mediaFtImage.length === 0
                   ? "//via.placeholder.com/350x150" : mediaFtImage[0].FileUrl}
               className={"db w-100 h5 br3 fit-cover " + hoverClass}
-              onMouseEnter={toggleHover}
-              onMouseLeave={toggleHover}
 
               alt={'Photo of ' + data.Title}/>
           <div>
